9beade0b3f8232d68e086215140de34f28352162
Hayk Zhamharyan <zhamharyanhayk5@gmail.com>
2025-07-28 20:13:26 +0400

    Fix the build.yaml PATH bug 5

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static HTML file ok
+// Serve static HTML file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
