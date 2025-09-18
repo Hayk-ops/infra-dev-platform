399b01ae616b68e539f55c2706446b1e014e9f6b
Hayk Zhamharyan <zhamharyanhayk5@gmail.com>
2025-09-18 18:07:49 +0400

    Adding server.js

--- app/server/server.js
+++ app/server/server.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
-  console.log(`My App is running on http://localhost:${port}`);
-});
+app.listen(process.env.PORT || 8080, '0.0.0.0', () => {
+  console.log(`My App is running on http://localhost:${process.env.PORT || 8080}`)
+})
